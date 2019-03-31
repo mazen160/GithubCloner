2b99bf980e840e80e2b4199784893f3dd6496ef2
Mazin Ahmed <mazin@mazinahmed.net>
2019-03-31 08:03:59 +0400

    removed test file

--- githubcloner_test.py
+++
@@ -1,7 +0,0 @@
-import githubcloner
-
-
-def test_get_repopath():
-    assert githubcloner.get_repopath("user", "repo", "none") == "repo"
-    assert githubcloner.get_repopath("user", "repo", "underscore") == "user_repo"
-    assert githubcloner.get_repopath("user", "repo", "directory") == "user/repo"
